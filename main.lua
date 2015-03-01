9d548bb5e4beeb3612857209cbd84e3306e50adc
John Murray <jmurrayufo@gmail.com>
2015-03-01 11:09:05 -0700

    Update startup and main

---
+++ main.lua
@@ -0,0 +1,26 @@
+--This script handles all other github scripts. It will remove old versions and download current ones. 
+
+local function get(repoFile,saveTo)
+   local download = http.get("https://raw.github.com/jmurrayufo/ComputerCraft/blob/master/"..repoFile) --This will make 'download' hold the contents of the file.
+   if download then --checks if download returned true or false
+      local handle = download.readAll() --Reads everything in download
+      download.close() --remember to close the download!
+      local file = fs.open(saveTo,"w") --opens the file defined in 'saveTo' with the permissions to write.
+      file.write(handle) --writes all the stuff in handle to the file defined in 'saveTo'
+      file.close() --remember to close the file!
+     else --if returned false
+      print("Unable to download the file "..repoFile)
+      print("Make sure you have the HTTP API enabled or")
+      print("an internet connection!")
+   end --end the if
+end --close the function
+
+if fs.exists("/BR") then 
+   fs.delete("/BR")
+   print("Old Version Found, deleteing")
+   sleep(1)
+end
+
+
+
+print("fin")
