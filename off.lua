8153e4da74ca9cc78beb8f643681166c53fd9b00
John Murray <jmurrayufo@gmail.com>
2015-03-01 17:00:50 -0700

    Insert needed tables

--- off.lua
+++ off.lua
@@ -1,4 +1,32 @@
+local turbines = {
+   "BigReactors-Turbine_15", --#1
+   "BigReactors-Turbine_4", --#2
+   "BigReactors-Turbine_5", --#3
+   "BigReactors-Turbine_6", --#4
+   "BigReactors-Turbine_7", --#5
+   "BigReactors-Turbine_8", --#6
+   "BigReactors-Turbine_9", --#7
+   "BigReactors-Turbine_10", --#8
+   "BigReactors-Turbine_11", --#9
+   "BigReactors-Turbine_12", --#10
+   "BigReactors-Turbine_13", --#11
+   "BigReactors-Turbine_14", --#12
+}
 
+local reactors = {
+   "BigReactors-Reactor_3", --#1
+   "BigReactors-Reactor_4", --#2
+   "BigReactors-Reactor_5", --#3
+   "BigReactors-Reactor_6", --#4
+   "BigReactors-Reactor_7", --#5
+   "BigReactors-Reactor_8", --#6
+   "BigReactors-Reactor_10", --#7
+   "BigReactors-Reactor_9", --#8
+   "BigReactors-Reactor_12", --#9
+   "BigReactors-Reactor_11", --#10
+   "BigReactors-Reactor_14", --#11
+   "BigReactors-Reactor_13", --#12
+}
 for i=1,#turbines do
    myScr.scroll(-1)
    myScr.setCursorPos(1,1)
